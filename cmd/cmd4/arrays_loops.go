ea71c9f8c3b1474008e694652a392d6a07b86855
Avantika Kesarwani <avikesar2013@gmail.com>
2024-11-04 14:07:36 +0530

    slices

--- cmd/cmd4/arrays_loops.go
+++ cmd/cmd4/arrays_loops.go
@@ -13,6 +13,15 @@ func main() {
 	arr := [...]int{4, 5, 6} //initializing array
 	fmt.Println(arr)
 
+	// slices
+	intslice := []int{8, 9, 10} //variable length
+	intslice = append(intslice, 7)
+	fmt.Println(intslice)
+
+	intslice2 := []int{2, 6}
+	intslice = append(intslice, intslice2...)
+	fmt.Println(intslice)
+
 }
 
 // arrays are declared as ==> var <array_name> [size]<data-type> or <arr_name> := [size]<data-type>
@@ -20,3 +29,12 @@ func main() {
 // arrays contain elements of the same data type.
 // arrays store elements in contigous memory locations.
 // arrays can be indexed.
+
+// slices are arrays with some additional functionalities.
+// in slice, i can omit the size such that unlike arrays it doesnt have a fixed length and i can add more elements to the sequence
+// using append() i can append an element to the end of an existing array.
+
+// capacity ( cap() ) and length ( len() ), cap ==> the total capacity of a slice, len ==> the no of elements in the slice.
+// eg. {8, 9, 10, 7, *, *}, here len=4 and cap=6
+
+// another way of making slice is by using the make function make(<datatype>[], len, cap)
